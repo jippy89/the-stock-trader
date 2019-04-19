1ae590f074317faac9909d5a7bdfc04fa2ee24a3
Jip Irfandy <irfandyjip98@gmail.com>
2019-04-20 00:43:59 +0700

    Create navbar!

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -1,27 +1,23 @@
 <template>
 	<el-menu
-		:default-active="activeIndex2"
-		class="el-menu-demo"
+		:default-active="activeIndex"
 		mode="horizontal"
 		@select="handleSelect"
 		background-color="#545c64"
 		text-color="#fff"
 		active-text-color="#ffd04b">
-		<el-menu-item index="1">Processing Center</el-menu-item>
-			<el-submenu index="2">
-			<template slot="title">Workspace</template>
-			<el-menu-item index="2-1">item one</el-menu-item>
-			<el-menu-item index="2-2">item two</el-menu-item>
-			<el-menu-item index="2-3">item three</el-menu-item>
-				<el-submenu index="2-4">
-				<template slot="title">item four</template>
-				<el-menu-item index="2-4-1">item one</el-menu-item>
-				<el-menu-item index="2-4-2">item two</el-menu-item>
-				<el-menu-item index="2-4-3">item three</el-menu-item>
-				</el-submenu>
-			</el-submenu>
-		<el-menu-item index="3" disabled>Info</el-menu-item>
-		<el-menu-item index="4"><a href="https://www.ele.me" target="_blank">Orders</a></el-menu-item>
+		<el-menu-item index="0">
+			<div slot="title">
+				<small>Icon</small>
+				THE STOCK TRADER
+			</div>
+		</el-menu-item>
+		<el-menu-item index="1">
+			<div slot="title">
+				Portfolio
+			</div>
+		</el-menu-item>
+		<el-menu-item index="2">Stocks</el-menu-item>
 	</el-menu>
 </template>
 
@@ -31,14 +27,19 @@
 	export default {
 		data() {
 			return {
-				activeIndex: '1',
-				activeIndex2: '1'
-			};
+				activeIndex: '0'
+			}
 		},
 		methods: {
 			handleSelect(key, keyPath) {
-			console.log(key, keyPath);
+				this.$router.push(this.$router.options.routes[key])
 			}
 		}
 	}
 </script>
+
+<style scoped>
+	.el-menu {
+		padding: 0 13px;
+	}
+</style>
