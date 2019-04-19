a7a2a60ba0a3b0ffcb4e1ed4f7b284cf605fd703
Jip Irfandy <irfandyjip98@gmail.com>
2019-04-19 23:41:49 +0700

    refactor(router): add route for 'Portfolio' and 'Stocks'

--- src/router.js
+++ src/router.js
@@ -12,12 +12,14 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/portfolio',
+      name: 'portfolio',
+      component: () => import(/* webpackChunkName: "portfolio" */ './views/portfolio/Portfolio.vue')
+    },
+    {
+      path: '/stocks',
+      name: 'stocks',
+      component: () => import(/* webpackChunkName: "stocks" */ './views/stocks/Stocks.vue')
     }
   ]
 })
