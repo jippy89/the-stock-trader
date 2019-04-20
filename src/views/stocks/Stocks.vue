fccf932877292dbe3e1caf87992191ecb3a48dcf
Jip Irfandy <irfandyjip98@gmail.com>
2019-04-20 21:47:16 +0700

    Move 'stocks' data to 'src/data/'

--- src/views/stocks/Stocks.vue
+++ src/views/stocks/Stocks.vue
@@ -12,17 +12,6 @@
 	export default {
 		components: {
 			Stock
-		},
-		data(){
-			return {
-				stocks: [
-					{id: 1, name: 'BMW', price: 110},
-					{id: 2, name: 'Google', price: 200},
-					{id: 3, name: 'Apple', price: 250},
-					{id: 4, name: 'Twitter', price: 250},
-					{id: 5, name: 'Audi', price: 120}
-				]
-			}
 		}
 	}
 </script>
