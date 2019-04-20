4407963df8339c4dd0f98ac600cca77af434224f
Jip Irfandy <irfandyjip98@gmail.com>
2019-04-20 21:29:35 +0700

    Create store for 'money'

--- src/store.js
+++ src/store.js
@@ -5,10 +5,20 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
-
+  	money: 3000
+  },
+  getters: {
+  	money: state => state.money
   },
   mutations: {
-
+  	addMoney(state, { amount }) {
+  		if(typeof amount === 'number')  this.state.money += amount
+		else console.log("Property 'amount' must be a number type")
+  	},
+  	subtractMoney(state, { amount }) {
+  		if(typeof amount === 'number')  this.state.money -= amount
+		else console.log("Property 'amount' must be a number type")
+  	}
   },
   actions: {
 
