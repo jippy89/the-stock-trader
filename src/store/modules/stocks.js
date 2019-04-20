6f7b94691b4c30b9a669e51813397ce2d230e3bc
Jip Irfandy <irfandyjip98@gmail.com>
2019-04-20 22:08:51 +0700

    test(store/stocks): add some basic functionality

--- src/store/modules/stocks.js
+++ src/store/modules/stocks.js
@@ -1,17 +1,32 @@
-const state = {
+import { stocks } from '@/data/stocks'
 
+const state = {
+	stocks: []
 }
 
 const getters = {
-
+	stocks: state => state.stocks
 }
 
 const mutations = {
-
+	'SET_STOCKS' (state, stocks) {
+		state.stocks = stocks
+	},
+	'RND_STOCKS' (state) {
+		
+	}
 }
 
 const actions = {
-
+	buyStock({ commit }, order ) {
+		commit()
+	},
+	initStocks({ commit }) {
+		commit('SET_STOCKS', stocks)
+	},
+	randomizeStocks({ commit }) {
+		commit('RND_STOCKS')
+	}
 }
 
 export default { state, getters, mutations, actions }
