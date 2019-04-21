5402d1ddc2fc7156f551caf7d11dffefa9014ef0
Jip Irfandy <irfandyjip98@gmail.com>
2019-04-22 03:20:08 +0700

    refactor(store): remove 'mutations' and 'getters'

--- src/store/store.js
+++ src/store/store.js
@@ -8,25 +8,6 @@ import portfolio from './modules/portfolio'
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  	money: 3000
-  },
-  getters: {
-  	money: state => state.money
-  },
-  mutations: {
-  	addMoney(state, { amount }) {
-  		if(typeof amount === 'number')  this.state.money += amount
-		else console.log("Property 'amount' must be a number type")
-  	},
-  	subtractMoney(state, { amount }) {
-  		if(typeof amount === 'number')  this.state.money -= amount
-		else console.log("Property 'amount' must be a number type")
-  	}
-  },
-  actions: {
-
-  },
   modules: [
     stocks,
     portfolio
