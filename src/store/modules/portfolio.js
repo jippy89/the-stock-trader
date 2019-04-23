eeb2bcf25fcec791875ab4ecd8d5ef58c534327f
Jip Irfandy <irfandyjip98@gmail.com>
2019-04-24 01:28:53 +0700

    refator(store/portfolio): reverse the '<' sign on 'SELL_STOCK'

--- src/store/modules/portfolio.js
+++ src/store/modules/portfolio.js
@@ -32,7 +32,7 @@ const mutations = {
 	},
 	'SELL_STOCK' ( state, { stockId, quantity, stockPrice } ) {
 		const record = state.stocks.find( element => element.id == stockId )
-		if( record.quantity < quantity ) {
+		if( record.quantity > quantity ) {
 			record.quantity -= quantity;
 		} else {
 			state.stocks.splice(state.stocks.indexOf(record), 1)
