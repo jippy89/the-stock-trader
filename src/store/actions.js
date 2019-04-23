dd6cb4bbbe84bc33858e0e2b9d4bb24d21d2ebd5
Jip Irfandy <irfandyjip98@gmail.com>
2019-04-24 03:30:12 +0700

    Create 'action.js'

---
+++ src/store/actions.js
@@ -0,0 +1,24 @@
+import '@/plugins/axios'
+
+export const loadData = ({ commit }) => {
+	axios.get('data.json')
+	.then( response => response.data )
+	.then( data => {
+		if(data) {
+			const stocks = data.stocks
+			const funds = data.funds
+			const stockPortfolio = data.stockPortfolio
+
+			const portfolio = {
+				stockPortfolio,
+				funds
+			}
+
+			commit('SET_STOCKS', stocks)
+			commit('SET_PORTFOLIO', portfolio)
+		}
+	} )
+	.catch( error => {
+		console.log(error)
+	})
+}
